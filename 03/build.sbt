0a22b01ec3ad8b2cca1cba51dd810b231dcb5892
Alexander Gerasimov <58679315+AlexG888@users.noreply.github.com>
2022-11-18 22:28:25 +0500

    HW3

---
+++ 03/build.sbt
@@ -0,0 +1,9 @@
+name := "03.Linear_regression"
+version := "0.0.1"
+scalaVersion := "2.13.10" 
+
+libraryDependencies ++= Seq(
+  "org.scalanlp" %% "breeze" % "1.3",
+  "org.scalanlp" %% "breeze-viz" % "1.3"
+)
+
---
+++ 03/src/main/scala/packages/model_tools.scala
@@ -0,0 +1,34 @@
+package packages
+
+import breeze.linalg.{DenseVector, DenseMatrix}
+import model.LinearRegression
+import breeze.numerics.abs
+import breeze.stats.mean
+import java.util.logging.{Logger}
+
+
+package object tools {
+  def mae(Y: DenseVector[Double], y: DenseVector[Double]): Double = {
+    mean(abs(Y - y))
+  }
+  def crossValidation(model: LinearRegression, data: DenseMatrix[Double], 
+                      numFolds: Int, logger: Logger): Unit = {
+    val step: Int = data.rows / numFolds
+    for (i <- 0 until numFolds) {
+      val trainIdxs: IndexedSeq[Int] = IndexedSeq.range(0, i * step) ++ IndexedSeq.range((i + 1) * step, data.rows)
+      val trainFold: DenseMatrix[Double] = data(trainIdxs, ::).toDenseMatrix
+      val xTrain: DenseMatrix[Double] = trainFold(::, 0 to -2)
+      val yTrain: DenseVector[Double] = trainFold(::, -1)
+      model.fit(xTrain, yTrain)
+
+      val validIdxs: IndexedSeq[Int] = IndexedSeq.range(i * step, (i + 1) * step)
+      val validFold: DenseMatrix[Double] = data(validIdxs, ::).toDenseMatrix
+      val xValid: DenseMatrix[Double] = validFold(::, 0 to -2)
+      val yValid: DenseVector[Double] = validFold(::, -1)
+      val maeScore: Double = mae(yValid, model.predict(xValid))
+      logger.info(s"Fold $i, MAE-score: $maeScore")
+    }
+  }
+}
+
+
